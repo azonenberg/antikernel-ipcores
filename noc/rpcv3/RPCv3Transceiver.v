2654243715781991f1aa0482622cb50976f7eaec
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-01-03 00:03:44 -0800

    RPCv3Transceiver: added initial RX datapath

--- noc/rpcv3/RPCv3Transceiver.v
+++ noc/rpcv3/RPCv3Transceiver.v
@@ -59,6 +59,7 @@
 		Assert rpc_fab_tx_en for one cycle
 		rpc_fab_tx_done goes high combinatorially for one cycle as the last word of the message is sent.
 		Do not change rpc_fab_tx_* until rpc_fab_tx_done goes low.
+		rpc_fab_tx_busy is high during the entire period of a send, including the "wait for rx_ready" phase.
 
 		Note that if DATA_WIDTH=128 and rpc_tx_ready is set, rpc_fab_tx_done will be asserted combinatorially.
 
@@ -70,30 +71,41 @@
 		Note that two messages can arrive on consecutive clocks (before rpc_fab_rx_ready can be deasserted)
 		if DATA_WIDTH=128, so any node receiving messages witha 128-bit bus must have a 2+ message receive FIFO.
 
-	RESOURCE USAGE (XST S6 post synthesis estimate, transmitter only)
+	RESOURCE USAGE (XST S6 post synthesis estimate, TX side)
 		Quiet
 			Width			FF			LUT
-			16				7			56
-			32				6			53
-			64				4			67
-			128				2			114
+			16				7			56		x
+			32				6			54
+			64				4			67		x
+			128				2			114		x
 
 		Noisy
 			Width			FF			LUT
-			16				9			53
-			32				6			37
-			64				3			67
-			128				1			2
+			16				9			53		x
+			32				6			37		x
+			64				3			67		x
+			128				1			2		x
+
+		RPCv2 (for comparison)
+			32				11			73
+
+	RESOURCE USAGE (XST S6 post synthesis estimate, RX side)
+		RPCv3
+			Width			FF			LUT
+			16				7			56		x
+
+		RPCv2 (for comparison)
+			32				11			73
  */
 module RPCv3Transceiver
 #(
 	//Data width (must be one of 16, 32, 64, 128).
-	parameter DATA_WIDTH = 128,
+	parameter DATA_WIDTH = 32,
 
 	//When zero, queued data is allowed to show up on rpc_tx_data in between packets.
 	//This can cause extra switching power consumption and clutters LA traces, but uses less LUTs.
 	//When nonzero, force rpc_tx_data to zero when not transmitting.
-	parameter QUIET_WHEN_IDLE = 0,
+	parameter QUIET_WHEN_IDLE = 1,
 
 	//This transceiver is always a leaf (node) port, no exceptions.
 	//We always send from this address.
@@ -114,6 +126,7 @@ module RPCv3Transceiver
 
 	//Fabric interface, outbound side
 	input wire					rpc_fab_tx_en,
+	output reg					rpc_fab_tx_busy		= 0,
 	input wire[15:0]			rpc_fab_tx_dst_addr,
 	input wire[7:0]				rpc_fab_tx_callnum,
 	input wire[2:0]				rpc_fab_tx_type,
@@ -124,6 +137,7 @@ module RPCv3Transceiver
 
 	//Fabric interface, inbound side
 	input wire					rpc_fab_rx_ready,
+	output reg 					rpc_fab_rx_busy		= 0,
 	output reg					rpc_fab_rx_en		= 0,
 	output reg[15:0]			rpc_fab_rx_src_addr	= 0,
 	output reg[7:0]				rpc_fab_rx_callnum	= 0,
@@ -182,16 +196,19 @@ module RPCv3Transceiver
 	wire[15:0]	tx_d7				= rpc_fab_tx_d2[15:0];
 
 	//If we couldn't send the first cycle, remember that we have a send queued up
-	reg			rpc_tx_pending		= 0;
+	reg			tx_pending			= 0;
+
+	//True if we want to send (either current or queued transmit request)
+	wire		tx_request			= tx_pending || rpc_fab_tx_en;
 
 	//True if we're starting a transmit this cycle (whether queued or fresh)
-	wire		rpc_tx_starting		= (rpc_tx_pending || rpc_fab_tx_en) && rpc_tx_ready;
+	wire		tx_starting			= tx_request && rpc_tx_ready;
 
 	//Position within the message (in DATA_WIDTH-bit units)
 	reg[CYCLE_BITS-1:0] tx_count	= 0;
 
 	//True if a transmit is in progress
-	wire		tx_active			= (tx_count != 0) || rpc_tx_starting;
+	wire		tx_active			= (tx_count != 0) || tx_starting;
 
 	generate
 
@@ -201,11 +218,14 @@ module RPCv3Transceiver
 			//All transmit logic is combinatorial
 			always @(*) begin
 
+				//We're only busy if we want to send, but can't yet
+				rpc_fab_tx_busy <= (tx_pending || (tx_count != 0) ) && !rpc_tx_ready;
+
 				//Send the message if we're starting to send
-				rpc_tx_en		<= rpc_tx_starting;
+				rpc_tx_en		<= tx_starting;
 
 				//We finish sending the same cycle we send
-				rpc_fab_tx_done	<= rpc_tx_starting;
+				rpc_fab_tx_done	<= tx_starting;
 
 				//Optionally squash output when not sending
 				if(QUIET_WHEN_IDLE && !tx_active)
@@ -221,12 +241,12 @@ module RPCv3Transceiver
 			always @(posedge clk) begin
 
 				//Clear pending messages once sent
-				if(rpc_tx_starting)
-					rpc_tx_pending	<= 0;
+				if(tx_starting)
+					tx_pending	<= 0;
 
 				//If we try to send when rx isn't ready, save it until they are
 				if(rpc_fab_tx_en && !rpc_tx_ready)
-					rpc_tx_pending	<= 1;
+					tx_pending	<= 1;
 
 			end
 
@@ -238,8 +258,11 @@ module RPCv3Transceiver
 			//Combinatorial transmit
 			always @(*) begin
 
+				//We're busy if we want to send, or are sending.
+				rpc_fab_tx_busy <= (tx_pending || (tx_count != 0) );
+
 				//Send the message if we're starting to send
-				rpc_tx_en		<= rpc_tx_starting;
+				rpc_tx_en		<= tx_starting;
 
 				//Optionally squash output when not sending
 				if(QUIET_WHEN_IDLE && !tx_active)
@@ -283,15 +306,15 @@ module RPCv3Transceiver
 
 				//Clear pending messages once sent
 				if(tx_active)
-					rpc_tx_pending	<= 0;
+					tx_pending	<= 0;
 
 				//If we try to send when rx isn't ready, save it until they are
 				if(rpc_fab_tx_en && !rpc_tx_ready)
-					rpc_tx_pending	<= 1;
+					tx_pending	<= 1;
 
 				//Increment word counter
 				if(tx_active)
-					tx_count		<= tx_count + 1'h1;
+					tx_count	<= tx_count + 1'h1;
 
 			end
 
@@ -301,5 +324,146 @@ module RPCv3Transceiver
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Receive path
+
+	//True if we are in the first cycle of an incoming message
+	wire					rx_starting		= (rpc_rx_en && rpc_rx_ready);
+
+	//Position within the message (in DATA_WIDTH-bit units)
+	reg[CYCLE_BITS-1:0]		rx_count		= 0;
+
+	//True if a receive is in progress
+	wire					rx_active		= (rx_count != 0) || rx_starting;
+
+	generate
+
+		//Set busy flag if we're doing stuff (mostly used by formal stuff)
+		always @(*) begin
+			rpc_fab_rx_busy		<= (rx_count != 0);
+		end
+
+		always @(posedge clk) begin
+
+			rpc_fab_rx_en		<= 0;
+
+			//Process incoming data words
+			if(rx_active) begin
+
+				//Whole message is on the wire right now
+				if(DATA_WIDTH == 128) begin
+					//127:112 are source address, ignore
+					rpc_fab_rx_src_addr	<= rpc_rx_data[111:96];
+					rpc_fab_rx_callnum	<= rpc_rx_data[95:88];
+					rpc_fab_rx_type		<= rpc_rx_data[87:85];
+					rpc_fab_rx_d0		<= rpc_rx_data[84:64];
+					rpc_fab_rx_d1		<= rpc_rx_data[63:32];
+					rpc_fab_rx_d2		<= rpc_rx_data[31:0];
+
+					//end of message
+					rpc_fab_rx_en		<= 1;
+				end
+
+				else begin
+
+					//Update word count as we move through the message
+					if(rx_starting)
+						rx_count		<= 1;
+					else
+						rx_count		<= rx_count + 1;
+
+					//Grab whatever fields are currently on the wire
+					case(rx_count)
+
+						0: begin
+
+							case(DATA_WIDTH)
+
+								16:	begin
+									//dest address, ignore
+								end
+
+								32: rpc_fab_rx_src_addr	<= rpc_rx_data[15:0];
+
+								64: begin
+									rpc_fab_rx_src_addr	<= rpc_rx_data[63:48];
+									rpc_fab_rx_callnum	<= rpc_rx_data[31:24];
+									rpc_fab_rx_type		<= rpc_rx_data[23:21];
+									rpc_fab_rx_d0		<= rpc_rx_data[20:0];
+								end
+
+							endcase
+
+						end
+
+						1: begin
+							case(DATA_WIDTH)
+								16:	begin
+									rpc_fab_rx_src_addr	<= rpc_rx_data;
+								end
+
+								32: begin
+									rpc_fab_rx_callnum	<= rpc_rx_data[31:24];
+									rpc_fab_rx_type		<= rpc_rx_data[23:21];
+									rpc_fab_rx_d0		<= rpc_rx_data[20:0];
+								end
+
+								64: begin
+									rpc_fab_rx_d1		<= rpc_rx_data[63:32];
+									rpc_fab_rx_d2		<= rpc_rx_data[31:0];
+
+									//end of message
+									rpc_fab_rx_en		<= 1;
+								end
+
+							endcase
+						end
+
+						2: begin
+							if(DATA_WIDTH == 32)
+								rpc_fab_rx_d1			<= rpc_rx_data;
+							else begin
+								rpc_fab_rx_callnum		<= rpc_rx_data[15:8];
+								rpc_fab_rx_type			<= rpc_rx_data[7:5];
+								rpc_fab_rx_d0[20:16]	<= rpc_rx_data[4:0];
+							end
+						end
+
+						3: begin
+							if(DATA_WIDTH == 32) begin
+								rpc_fab_rx_d2			<= rpc_rx_data;
+
+								//end of message
+								rpc_fab_rx_en		<= 1;
+							end
+							else
+								rpc_fab_rx_d0[15:0]		<= rpc_rx_data;
+						end
+
+						//If we get here we're a 16-bit message, so no more conditionals needed
+						4: rpc_fab_rx_d1[31:16]	<= rpc_rx_data;
+						5: rpc_fab_rx_d1[15:0]	<= rpc_rx_data;
+						6: rpc_fab_rx_d2[31:16]	<= rpc_rx_data;
+						7: begin
+							rpc_fab_rx_d2[15:0]	<= rpc_rx_data;
+
+							//end of message
+							rpc_fab_rx_en		<= 1;
+						end
+
+					endcase
+				end
+			end
+
+			//In any datapath width, source address is the first word
+			//if(rx_starting)
+			//	rpc_fab_rx_src_addr		<= rpc_rx_data[DATA_WIDTH-1 : DATA_WIDTH-16];
+
+		end
+
+		//placeholder: always ready to receive
+		always @(*) begin
+			rpc_rx_ready	<= 1;
+		end
+
+	endgenerate
 
 endmodule
