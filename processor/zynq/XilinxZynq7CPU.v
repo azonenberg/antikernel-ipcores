335331f9ecaf502defa80080f5f3312f9dff72b6
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-02-14 13:15:25 -0800

    XilinxZynq7CPU: Fixed typo in filename

--- processor/zynq/XilinxZynq7CPU.v
+++ processor/zynq/XilinxZynq7CPU.v
@@ -44,7 +44,7 @@ module XilinxZynq7CPU(
 	//Note that these pins are INPUTS.
 	//The multiple-driver warning is a false positive (see AR#50430).
 	//The __nowarn_528_ prefix will make Splash filter it out
-	input wire __nowarn_528_cpu_clk,
+	output wire cpu_clk,
 	input wire __nowarn_528_cpu_por_n,
 	input wire __nowarn_528_cpu_srst_n
 	);
@@ -55,7 +55,7 @@ module XilinxZynq7CPU(
 	PS7 cpu(
 
 		//CPU clock and reset
-		.PSCLK(__nowarn_528_cpu_clk),
+		.PSCLK(cpu_clk),
 		.PSPORB(__nowarn_528_cpu_por_n),
 		.PSSRSTB(__nowarn_528_cpu_srst_n),
 
