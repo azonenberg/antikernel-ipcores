15d65d901b842adbb28bb6d9e9ff883d9d045f62
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-03-16 22:46:02 -0700

    JtagUserIdentifier: initial version

---
+++ debug/JtagUserIdentifier.v
@@ -0,0 +1,95 @@
+`default_nettype none
+`timescale 1ns / 1ps
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2017 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@brief Reports a fixed 32-bit identifier in the USER1 register.
+
+	We could use USERCODE for this, but that would prevent it from being used by an end-user design.
+ */
+module JtagUserIdentifier #(
+	parameter IDCODE_VID = 24'h000000,
+	parameter IDCODE_PID = 8'h00
+)();
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// The TAP interface for discovery (DEBUG_IDCODE register)
+
+	reg[31:0]	tap_shreg = 0;
+
+	wire		tap_active;
+	wire		tap_shift;
+	wire		tap_clear;
+	wire		tap_tck_raw;
+	wire		tap_tck_bufh;
+
+	//The TAP itself
+	JtagTAP #(
+		.USER_INSTRUCTION(1)
+	) tap_tap (
+		.instruction_active(tap_active),
+		.state_capture_dr(tap_clear),
+		.state_reset(),
+		.state_runtest(),
+		.state_shift_dr(tap_shift),
+		.state_update_dr(),
+		.tck(tap_tck_raw),
+		.tck_gated(),
+		.tms(),
+		.tdi(),
+		.tdo(tap_shreg[0])
+	);
+
+	//Buffer the clock b/c ISE is derpy and often won't instantiate a buffer (woo skew!)
+	//TODO: according to comments in older code BUFHs here sometimes won't work in spartan6?
+	ClockBuffer #(
+		.TYPE("LOCAL"),
+		.CE("NO")
+	) tap_tck_clkbuf (
+		.clkin(tap_tck_raw),
+		.clkout(tap_tck_bufh),
+		.ce(1'b1)
+	);
+
+	//The actual shift register
+	always @(posedge tap_tck_bufh) begin
+
+		if(!tap_active) begin
+		end
+
+		else if(tap_clear)
+			tap_shreg	<= {IDCODE_VID, IDCODE_PID};
+
+		else if(tap_shift)
+			tap_shreg	<= {1'b1, tap_shreg[31:1]};
+
+	end
+
+endmodule
