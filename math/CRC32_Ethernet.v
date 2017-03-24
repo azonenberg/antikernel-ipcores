80c791dacd4ad18e2a624a0dc86e1eeb458e556e
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-03-24 00:50:59 -0700

    CRC32_Ethernet: Removed trailing spaces

--- math/CRC32_Ethernet.v
+++ math/CRC32_Ethernet.v
@@ -32,23 +32,23 @@
 	@file
 	@author Andrew D. Zonenberg
 	@brief Ethernet CRC-32 (derived from easics.com generator)
-	
+
 	Original license:
-	
+
 	Copyright (C) 1999-2008 Easics NV.
 	 This source file may be used and distributed without restriction
 	 provided that this copyright statement is not removed from the file
 	 and that any derivative work contains the original copyright notice
 	 and the associated disclaimer.
-	
+
 	 THIS SOURCE FILE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS
 	 OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
 	 WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-	
+
 	 Purpose : synthesizable CRC function
 	   * polynomial: (0 1 2 4 5 7 8 10 11 12 16 22 23 26 32)
 	   * data width: 8
-	
+
 	 Info : tools@easics.be
 	        http://www.easics.com
  */
@@ -56,42 +56,42 @@ module EthernetCRC32(clk, reset, update, din, crc_flipped);
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// I/O declarations
-	
+
 	input wire clk;
 	input wire reset;
-	
+
 	input wire update;
 	input wire[7:0] din;
-	
+
 	reg[31:0] crc = 0;
-	
+
 	output wire[31:0] crc_flipped;
-	
+
 	wire[31:0] crc_not = ~crc;
 	assign crc_flipped =
 	{
 		crc_not[24], crc_not[25], crc_not[26], crc_not[27],
 		crc_not[28], crc_not[29], crc_not[30], crc_not[31],
-		
+
 		crc_not[16], crc_not[17], crc_not[18], crc_not[19],
 		crc_not[20], crc_not[21], crc_not[22], crc_not[23],
-	
+
 		crc_not[8], crc_not[9], crc_not[10], crc_not[11],
 		crc_not[12], crc_not[13], crc_not[14], crc_not[15],
-		
+
 		crc_not[0], crc_not[1], crc_not[2], crc_not[3],
-		crc_not[4], crc_not[5], crc_not[6], crc_not[7]		
+		crc_not[4], crc_not[5], crc_not[6], crc_not[7]
 	};
-	
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// The actual CRC function
-	
-	wire[7:0] din_flipped = 
+
+	wire[7:0] din_flipped =
 	{
 		din[0], din[1], din[2], din[3],
 		din[4], din[5], din[6], din[7]
 	};
-	
+
 	always @(posedge clk) begin
 		if(reset)
 			crc <= 'hffffffff;
