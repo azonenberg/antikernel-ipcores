ca9ca42c0275134340657e4ccbb6516b1d1464d2
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-10-26 22:23:58 -0700

    Lots more work on SFDP parsing.

--- memory/qspi_flash/SFDPParser.v
+++ memory/qspi_flash/SFDPParser.v
@@ -229,11 +229,6 @@ module SFDPParser(
 	localparam STATE_HANG			= 255;
 
 	reg[7:0]	state		= STATE_BOOT_WAIT;
-	reg[7:0]	bytecount	= 0;
-
-	reg			sfdp_avail	= 0;
-	reg[8:0]	sfdp_addr	= 0;
-	reg[7:0]	sfdp_data	= 0;
 
 	reg			phdrs_done	= 0;
 
@@ -244,6 +239,7 @@ module SFDPParser(
 	reg[23:0]	basic_params_offset		= 0;
 	reg[7:0]	basic_params_len		= 0;
 
+	reg[8:0]	sfdp_addr			= 0;
 	wire[5:0]	phdr_num			= sfdp_addr[8:3] - 6'd1;
 	wire[2:0]	phdr_boff			= sfdp_addr[2:0];
 
@@ -259,7 +255,6 @@ module SFDPParser(
 	localparam SFDP_REASON_BAD_MAJOR_VERSION	= 8'h2;
 
     always @(posedge clk) begin
-		sfdp_avail			<= 0;
 		read_request		<= 0;
 		read_finish_request	<= 0;
 
@@ -275,18 +270,15 @@ module SFDPParser(
 				if(la_ready && !read_busy) begin
 					read_request		<= 1;
 					read_addr			<= 0;
-					sfdp_addr			<= 9'h1ff;		//max, will overflow to zero
+					sfdp_addr			<= 0;		//max, will overflow to zero
 					state				<= STATE_HEADER_PARSE;
 				end
 			end	//end STATE_BOOT_WAIT
 
 			STATE_HEADER_PARSE: begin
-				if(read_done) begin
 
-					//Send this word on to get analyzed
-					sfdp_addr	<= sfdp_addr + 1'h1;
-					sfdp_data	<= read_data;
-					sfdp_avail	<= 1;
+				if(read_done) begin
+					sfdp_addr				<= sfdp_addr + 1'h1;
 
 					//If we got a bad PHDR, die
 					if(sfdp_bad) begin
@@ -311,10 +303,9 @@ module SFDPParser(
 				if(!read_busy) begin
 					read_request		<= 1;
 					read_addr			<= basic_params_offset;
-					bytecount			<= 0;
 					state				<= STATE_PARSE_BASIC;
 
-					sfdp_addr			<= 9'h1ff;
+					sfdp_addr			<= 9'h0;
 				end
 			end	//end STATE_READ_BASIC
 
@@ -322,10 +313,6 @@ module SFDPParser(
 				if(read_done) begin
 
 					sfdp_addr			<= sfdp_addr + 1'h1;
-					sfdp_data			<= read_data;
-					sfdp_avail			<= 1;
-
-					bytecount			<= bytecount + 1'h1;
 
 					//If we just read the last byte of the descriptor, we're done.
 					//Note that basic_params_len is a count of DWORDs so we have to multiply by 4!
@@ -403,7 +390,7 @@ module SFDPParser(
 
 			end
 
-			if(sfdp_avail) begin
+			if(read_done) begin
 
 				case(sfdp_state)
 
@@ -415,28 +402,28 @@ module SFDPParser(
 
 							//Signature: "SFDP"
 							0: begin
-								if(sfdp_data != "S") begin
+								if(read_data != "S") begin
 									sfdp_bad			<= 1;
 									sfdp_fail_reason	<= SFDP_REASON_BAD_HEADER;
 								end
 							end
 
 							1: begin
-								if(sfdp_data != "F") begin
+								if(read_data != "F") begin
 									sfdp_bad			<= 1;
 									sfdp_fail_reason	<= SFDP_REASON_BAD_HEADER;
 								end
 							end
 
 							2: begin
-								if(sfdp_data != "D") begin
+								if(read_data != "D") begin
 									sfdp_bad			<= 1;
 									sfdp_fail_reason	<= SFDP_REASON_BAD_HEADER;
 								end
 							end
 
 							3: begin
-								if(sfdp_data != "P") begin
+								if(read_data != "P") begin
 									sfdp_bad			<= 1;
 									sfdp_fail_reason	<= SFDP_REASON_BAD_HEADER;
 								end
@@ -444,12 +431,12 @@ module SFDPParser(
 
 							//SFDP spec minor revision
 							4: begin
-								sfdp_minor_rev			<= sfdp_data;
+								sfdp_minor_rev			<= read_data;
 							end
 
 							//SFDP spec major revision
 							5: begin
-								if(sfdp_data != 8'h1) begin
+								if(read_data != 8'h1) begin
 									sfdp_bad			<= 1;
 									sfdp_fail_reason	<= SFDP_REASON_BAD_MAJOR_VERSION;
 								end
@@ -457,7 +444,7 @@ module SFDPParser(
 
 							//Number of parameter headers
 							6: begin
-								sfdp_num_phdrs		<= sfdp_data;
+								sfdp_num_phdrs		<= read_data;
 							end
 
 							//Reserved field, ignore (but move on to the parameter headers)
@@ -479,24 +466,24 @@ module SFDPParser(
 						case(phdr_boff)
 
 							//Byte 0: PHDR ID (LSB)
-							0:	sfdp_current_param[7:0]		<= sfdp_data;
+							0:	sfdp_current_param[7:0]		<= read_data;
 
 							//Byte 1/2: PHDR minor/major rev
-							1:	sfdp_param_rev[7:0]			<= sfdp_data;
-							2:	sfdp_param_rev[15:8]		<= sfdp_data;
+							1:	sfdp_param_rev[7:0]			<= read_data;
+							2:	sfdp_param_rev[15:8]		<= read_data;
 
 							//Byte 3: Length of this parameter table (in DWORDs)
-							3:	sfdp_param_len				<= sfdp_data;
+							3:	sfdp_param_len				<= read_data;
 
 							//Byte 4/5/6: parameter offset
-							4:	sfdp_param_offset[7:0]		<= sfdp_data;
-							5:	sfdp_param_offset[15:8]		<= sfdp_data;
-							6:	sfdp_param_offset[23:16]	<= sfdp_data;
+							4:	sfdp_param_offset[7:0]		<= read_data;
+							5:	sfdp_param_offset[15:8]		<= read_data;
+							6:	sfdp_param_offset[23:16]	<= read_data;
 
 							//Byte 7: PHDR ID (MSB)
 							7: begin
 								sfdp_param_valid			<= 1;
-								sfdp_current_param[15:8]	<= sfdp_data;
+								sfdp_current_param[15:8]	<= read_data;
 							end
 
 						endcase
@@ -514,46 +501,148 @@ module SFDPParser(
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // What we've all been waiting for: the actual output of the parsing!
 
+    //Addressing
+    reg			has_3byte_addr		= 0;
+    reg			has_4byte_addr		= 0;
+
+	//Clocking
+    reg			has_ddr_mode		= 0;
+
+	//Memory size
+    reg[15:0]	capacity_mbits		= 0;
+
+	//Fast read (1-1-4)
     reg			has_114_read		= 0;
     reg[7:0]	insn_114_read		= 0;
     reg[2:0]	modeclk_114_read	= 0;
     reg[4:0]	dummyclk_114_read	= 0;
 
+	//Fast read (1-4-4)
     reg			has_144_read		= 0;
     reg[7:0]	insn_144_read		= 0;
     reg[2:0]	modeclk_144_read	= 0;
     reg[4:0]	dummyclk_144_read	= 0;
 
+	//Fast read (4-4-4)
     reg			has_444_read		= 0;
     reg[7:0]	insn_444_read		= 0;
     reg[2:0]	modeclk_444_read	= 0;
     reg[4:0]	dummyclk_444_read	= 0;
 
-    //we don't care about dual, go quad or go home!
-    reg			has_ddr_mode		= 0;
-    reg			has_3byte_addr		= 0;
-    reg			has_4byte_addr		= 0;
+    //Erasing
+    reg[7:0]	erase_type1_insn	= 0;
+    reg[15:0]	erase_type1_kbits	= 0;
+    reg[15:0]	erase_type1_ms		= 0;	//typical delay
 
-    reg[15:0]	capacity_mbits		= 0;
+    reg[7:0]	erase_type2_insn	= 0;
+    reg[15:0]	erase_type2_kbits	= 0;
+    reg[15:0]	erase_type2_ms		= 0;
+
+    reg[7:0]	erase_type3_insn	= 0;
+    reg[15:0]	erase_type3_kbits	= 0;
+    reg[15:0]	erase_type3_ms		= 0;
+
+    reg[7:0]	erase_type4_insn	= 0;
+    reg[15:0]	erase_type4_kbits	= 0;
+    reg[15:0]	erase_type4_ms		= 0;
+
+    reg[31:0]	erase_chip_ms			= 0;
+
+    //Programming
+    reg[6:0]	program_first_byte_us	= 0;
+    reg[6:0]	program_per_byte_us		= 0;
+    reg[15:0]	program_page_us			= 0;
+    reg[15:0]	page_size_bytes			= 0;
+
+    //Time multipliers
+    reg[4:0]	erase_typ_to_max	= 0;
+    reg[4:0]	program_typ_to_max	= 0;
+
+    //Status register polling
+    reg			busy_poll_via_flagstatreg	= 0;	//0x70
+    reg			busy_poll_via_statreg		= 0;	//0x05
+
+	/*
+		Quad enable bit
+			Read status register 1 with 0x05
+
+			0	No QE bit, just use opcode to select.
+				Need to leave DQ3 high between instructions.
+
+			1	QE is bit 1 of status register 2.
+				Set via 2-byte Write Status (0x01) command
+
+			2	QE is bit 6 of status register 1.
+				Set via 1-byte Write Status command
+
+			3	QE is bit 7 of status register 2.
+				Set via 1-byte Write Status Register 2 command
+				Read status register with 0x3E
+
+			4	QE is bit 1 of status register 2.
+				Set via 2-byte Write Status command.
+				Same as state 1 except for truncated write handling
+
+			5	QE is bit 1 of status register 2.
+				Read with 0x35
+				Write with 2-byte Write Status command
+
+			6/7	Reserved
+	 */
+    reg[2:0]	quad_enable_type			= 0;
+
+	//Methods of entering 4-4-4 mode
+    reg			quad_qe_38	= 0;			//Set QE, then 0x38
+    reg			quad_38		= 0;			//Issue 0x38
+    reg			quad_35		= 0;			//Issue 0x35
+    reg			quad_6561	= 0;			//Issue 0x65 then 61 read-modify-write
+    reg			quad_6571	= 0;			//Issue 0x65/address then 71/address read-modify-write
+
+    //Methods of leaving 4-4-4 mode
+    reg			qexit_ff	= 0;			//Issue 0xff
+    reg			qexit_f5	= 0;			//Issue 0xf5
+    reg			qexit_reset	= 0;			//Issue 0x66/99 to reset
+    reg			qexit_6571	= 0;			//Issue 0x65/address then 71/address read-modify-write
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Parse the basic flash parameter table
 
+	reg[6:0]	basic_dword_addr	= 0;
     reg[31:0]	basic_dword			= 0;
     reg			basic_dword_valid	= 0;
 
-    wire[6:0]	basic_dword_addr	= sfdp_addr[8:2];
+	//Carve out a few bytes and name them
+    wire[7:0]	basic_dword_b3		= basic_dword[23:16];
+    wire[7:0]	basic_dword_b0		= basic_dword[7:0];
+
+	//Subtract 10 with saturation to zero (for erase block info)
+    reg[7:0]	basic_dword_b3_m10	= 0;
+    reg[7:0]	basic_dword_b0_m10	= 0;
+    always @(*) begin
+		if(basic_dword_b3 < 10)
+			basic_dword_b3_m10		<= 0;
+		else
+			basic_dword_b3_m10		<= basic_dword_b3 - 7'd10;
+
+		if(basic_dword_b0 < 10)
+			basic_dword_b0_m10		<= 0;
+		else
+			basic_dword_b0_m10		<= basic_dword_b0 - 7'd10;
+
+    end
 
 	always @(posedge clk) begin
 
 		basic_dword_valid			<= 0;
 
 		//Shove bytes into 32-bit chunks
-		if(parsing_basic_params && sfdp_avail) begin
+		if(parsing_basic_params && read_done) begin
 
-			basic_dword	<= {sfdp_data, basic_dword[31:8]};
-			if(sfdp_addr[1:0] == 3)
+			basic_dword	<= {read_data, basic_dword[31:8]};
+			if(sfdp_addr[1:0] == 3) begin
+				basic_dword_addr	<= sfdp_addr[8:2];
 				basic_dword_valid	<= 1;
+			end
 
 		end
 
@@ -563,6 +652,9 @@ module SFDPParser(
 			case(basic_dword_addr)
 
 				0: begin
+
+					insn_114_read	<= 8'haa;
+					insn_444_read	<= basic_dword[7:0];	//FIXME
 
 					//bits 7:0: mostly legacy, ignore
 					//TODO: parse this in case of an old SFDP version?
@@ -598,8 +690,12 @@ module SFDPParser(
 				1: begin
 
 					//Bit 31 = log mode
+					//Capacity is 2^n *bits*
 					if(basic_dword[31])
-						capacity_mbits	<= basic_dword[31:19];
+						capacity_mbits	<= 1'b1 << (basic_dword[15:0] - 20);
+
+					//Not log mode
+					//Capacity is num_bits - 1
 					else
 						capacity_mbits	<= basic_dword[30:20] + 1;
 
@@ -635,6 +731,142 @@ module SFDPParser(
 				end
 
 				//Dword 7: Erase configuration
+				//Erase sizes are 2^n *bytes*.
+				//2^(n-10) = kbytes
+				//2^(n-7) = kbits
+				7: begin
+					erase_type2_insn	<= basic_dword[31:24];
+					if(basic_dword_b3_m10[3:0])
+						erase_type2_kbits	<= 1'b1 << basic_dword_b3_m10[3:0];
+					erase_type1_insn	<= basic_dword[15:8];
+					if(basic_dword_b0_m10[3:0])
+						erase_type1_kbits	<= 1'b1 << basic_dword_b0_m10[3:0];
+				end
+
+				//Dword 8: Erase configuration
+				8: begin
+					erase_type4_insn	<= basic_dword[31:24];
+					if(basic_dword_b3_m10[3:0])
+						erase_type4_kbits	<= 1'b1 << basic_dword_b3_m10[3:0];
+					erase_type3_insn	<= basic_dword[15:8];
+					if(basic_dword_b0_m10[3:0])
+						erase_type3_kbits	<= 1'b1 << basic_dword_b0_m10[3:0];
+				end
+
+				//Dword 9: Erase run times
+				9: begin
+
+					case(basic_dword[31:30])
+						0:	erase_type4_ms	<= basic_dword[29:25];
+						1:	erase_type4_ms	<= {basic_dword[29:25], 4'h0};
+						2:	erase_type4_ms	<= {basic_dword[29:25], 7'h0};
+						3:	erase_type4_ms	<= {basic_dword[29:25], 10'h0};
+					endcase
+
+					case(basic_dword[24:23])
+						0:	erase_type3_ms	<= basic_dword[22:18];
+						1:	erase_type3_ms	<= {basic_dword[22:18], 4'h0};
+						2:	erase_type3_ms	<= {basic_dword[22:18], 7'h0};
+						3:	erase_type3_ms	<= {basic_dword[22:18], 10'h0};
+					endcase
+
+					case(basic_dword[17:16])
+						0:	erase_type2_ms	<= basic_dword[15:11];
+						1:	erase_type2_ms	<= {basic_dword[15:11], 4'h0};
+						2:	erase_type2_ms	<= {basic_dword[15:11], 7'h0};
+						3:	erase_type2_ms	<= {basic_dword[15:11], 10'h0};
+					endcase
+
+					case(basic_dword[10:9])
+						0:	erase_type1_ms	<= basic_dword[8:4];
+						1:	erase_type1_ms	<= {basic_dword[8:4], 4'h0};
+						2:	erase_type1_ms	<= {basic_dword[8:4], 7'h0};
+						3:	erase_type1_ms	<= {basic_dword[8:4], 10'h0};
+					endcase
+
+					erase_typ_to_max		<= { basic_dword[3:0], 1'b1 };
+
+				end
+
+				//Dword 10: Erase/program run times, page info
+				10: begin
+
+					//Full chip erase run time
+					case(basic_dword[30:29])
+						0:	erase_chip_ms	<= {basic_dword[28:24], 4'h0};
+						1:	erase_chip_ms	<= {basic_dword[28:24], 8'h0};
+						2:	erase_chip_ms	<= {basic_dword[28:24], 12'h0};
+						3:	erase_chip_ms	<= {basic_dword[28:24], 16'h0};
+					endcase
+
+					//Time to program one byte after the first byte
+					if(basic_dword[23])
+						program_per_byte_us	<= {basic_dword[22:19], 3'h0};
+					else
+						program_per_byte_us	<= basic_dword[22:19];
+
+					//Time to program the first byte in a block
+					if(basic_dword[18])
+						program_first_byte_us	<= {basic_dword[17:14], 3'h0};
+					else
+						program_first_byte_us	<= basic_dword[17:14];
+
+					//Time to program a whole page
+					if(basic_dword[13])
+						program_page_us			<= {basic_dword[12:8], 6'h0};
+					else
+						program_page_us			<= {basic_dword[12:8], 3'h0};
+
+					page_size_bytes				<= 1'b1 << basic_dword[7:4];
+
+					program_typ_to_max			<= { basic_dword[3:0], 1'b1 };
+				end
+
+				//Dword 11: Suspend stuff
+				11: begin
+					//For now we don't support suspend of anything. Ignore it.
+				end
+
+				//Dword 12: Suspend stuff
+				12: begin
+					//For now we don't support suspend of anything. Ignore it.
+				end
+
+				//Dword 13: Power-down etc
+				13: begin
+					//Power down config stuff
+
+					busy_poll_via_flagstatreg	<= basic_dword[3];
+					busy_poll_via_statreg		<= basic_dword[2];
+				end
+
+				//Dword 14: More config stuff
+				14: begin
+					//Hold/reset disable TODO
+
+					quad_enable_type			<= basic_dword[22:20];
+
+					//0-4-4 XIP mode: not used, ignore
+
+					//4-4-4 mode enable
+					quad_qe_38					<= basic_dword[4];
+					quad_38						<= basic_dword[5];
+					quad_35						<= basic_dword[6];
+					quad_6571					<= basic_dword[7];
+					quad_6561					<= basic_dword[8];
+					qexit_ff					<= basic_dword[0];
+					qexit_f5					<= basic_dword[1];
+					qexit_6571					<= basic_dword[2];
+					qexit_reset					<= basic_dword[3];
+				end
+
+				//Dword 15: 4-byte-address mode config
+				15: begin
+
+					//Exit 4-byte mode not supported.
+					//If the device has a 4-byte mode we always use it.
+
+				end
 
 			endcase
 
@@ -646,7 +878,7 @@ module SFDPParser(
 
 	//intclk is 66.5 MHz for current prototype at last measurement
 	RedTinUartWrapper #(
-		.WIDTH(256),
+		.WIDTH(384),
 		.DEPTH(2048),
 		//.UART_CLKDIV(16'd868),	//115200 @ 100 MHz
 		.UART_CLKDIV(16'd577),		//115200 @ 66.5 MHz
@@ -656,38 +888,43 @@ module SFDPParser(
 				"DEBUGROM", 				8'h0, 8'h01, 8'h00,
 				32'd15037,			//period of internal clock, in ps
 				32'd2048,			//Capture depth (TODO auto-patch this?)
-				32'd256,			//Capture width (TODO auto-patch this?)
+				32'd384,			//Capture width (TODO auto-patch this?)
 				{ "parsing_master_header",	8'h0, 8'h1,  8'h0 },
 				{ "parsing_basic_params",	8'h0, 8'h1,  8'h0 },
-				{ "sfdp_data",				8'h0, 8'h8,  8'h0 },
-				{ "sfdp_addr",				8'h0, 8'h9,  8'h0 },
-				{ "sfdp_bad",				8'h0, 8'h1,  8'h0 },
 				{ "phdr_num",				8'h0, 8'h6,  8'h0 },
-				{ "phdr_boff",				8'h0, 8'h3,  8'h0 },
-				{ "sfdp_state",				8'h0, 8'h3,  8'h0 },
-				{ "sfdp_current_param",		8'h0, 8'h10,  8'h0 },
-				{ "sfdp_param_rev",			8'h0, 8'h10,  8'h0 },
-				{ "sfdp_param_len",			8'h0, 8'h8,  8'h0 },
-				{ "sfdp_param_offset",		8'h0, 8'h18,  8'h0 },
-				{ "basic_dword_valid",		8'h0, 8'h1,  8'h0 },
-				{ "basic_dword_addr",		8'h0, 8'h7,  8'h0 },
-				{ "basic_dword",			8'h0, 8'h20, 8'h0 },
-				{ "has_114_read",			8'h0, 8'h1,  8'h0 },
-				{ "insn_114_read",			8'h0, 8'h8,  8'h0 },
-				{ "modeclk_114_read",		8'h0, 8'h3,  8'h0 },
-				{ "dummyclk_114_read",		8'h0, 8'h5,  8'h0 },
-				{ "has_144_read",			8'h0, 8'h1,  8'h0 },
-				{ "insn_144_read",			8'h0, 8'h8,  8'h0 },
-				{ "modeclk_144_read",		8'h0, 8'h3,  8'h0 },
-				{ "dummyclk_144_read",		8'h0, 8'h5,  8'h0 },
-				{ "has_444_read",			8'h0, 8'h1,  8'h0 },
-				{ "insn_444_read",			8'h0, 8'h8,  8'h0 },
-				{ "modeclk_444_read",		8'h0, 8'h3,  8'h0 },
-				{ "dummyclk_444_read",		8'h0, 8'h5,  8'h0 },
-				{ "has_ddr_mode",			8'h0, 8'h1,  8'h0 },
 				{ "has_3byte_addr",			8'h0, 8'h1,  8'h0 },
 				{ "has_4byte_addr",			8'h0, 8'h1,  8'h0 },
-				{ "capacity_mbits",			8'h0, 8'h10,  8'h0 }
+				{ "erase_type1_insn",		8'h0, 8'h8,  8'h0 },
+				{ "erase_type1_mbits",		8'h0, 8'h10, 8'h0 },
+				{ "erase_type1_ms",			8'h0, 8'h10, 8'h0 },
+				{ "erase_type2_insn",		8'h0, 8'h8,  8'h0 },
+				{ "erase_type2_kbits",		8'h0, 8'h10, 8'h0 },
+				{ "erase_type2_ms",			8'h0, 8'h10, 8'h0 },
+				{ "erase_type3_insn",		8'h0, 8'h8,  8'h0 },
+				{ "erase_type3_kbits",		8'h0, 8'h10, 8'h0 },
+				{ "erase_type3_ms",			8'h0, 8'h10, 8'h0 },
+				{ "erase_type4_insn",		8'h0, 8'h8,  8'h0 },
+				{ "erase_type4_kbits",		8'h0, 8'h10, 8'h0 },
+				{ "erase_type4_ms",			8'h0, 8'h10, 8'h0 },
+				{ "erase_typ_to_max",		8'h0, 8'h5,  8'h0 },
+				{ "erase_chip_ms",			8'h0, 8'h20, 8'h0 },
+				{ "program_first_byte_us",	8'h0, 8'h7,  8'h0 },
+				{ "program_per_byte_us",	8'h0, 8'h7,  8'h0 },
+				{ "program_page_us",		8'h0, 8'h10, 8'h0 },
+				{ "page_size_bytes",		8'h0, 8'h10, 8'h0 },
+				{ "program_typ_to_max",		8'h0, 8'h5, 8'h0 },
+				{ "busy_poll_via_flagstatreg", 8'h0, 8'h1,  8'h0 },
+				{ "busy_poll_via_statreg",  8'h0, 8'h1,  8'h0 },
+				{ "quad_enable_type",		8'h0, 8'h3,  8'h0 },
+				{ "quad_qe_38",				8'h0, 8'h1,  8'h0 },
+				{ "quad_38",				8'h0, 8'h1,  8'h0 },
+				{ "quad_35",				8'h0, 8'h1,  8'h0 },
+				{ "quad_6561",				8'h0, 8'h1,  8'h0 },
+				{ "quad_6571",				8'h0, 8'h1,  8'h0 },
+				{ "qexit_ff",				8'h0, 8'h1,  8'h0 },
+				{ "qexit_f5",				8'h0, 8'h1,  8'h0 },
+				{ "qexit_6571",				8'h0, 8'h1,  8'h0 },
+				{ "qexit_reset",			8'h0, 8'h1,  8'h0 }
 			}
 		)
 	) analyzer (
@@ -696,37 +933,42 @@ module SFDPParser(
 		.din({
 				parsing_master_header,	//1
 				parsing_basic_params,	//1
-				sfdp_data,				//8
-				sfdp_addr,				//9
-				sfdp_bad,				//1
 				phdr_num,				//6
-				phdr_boff,				//3
-				sfdp_state,				//3
-				sfdp_current_param,		//16
-				sfdp_param_rev,			//16
-				sfdp_param_len,			//8
-				sfdp_param_offset,		//24
-				basic_dword_valid,		//1
-				basic_dword_addr,		//7
-				basic_dword,			//32
-				has_114_read,			//1
-				insn_114_read,			//8
-				modeclk_114_read,		//3
-				dummyclk_114_read,		//5
-				has_144_read,			//1
-				insn_144_read,			//8
-				modeclk_144_read,		//3
-				dummyclk_144_read,		//5
-				has_444_read,			//1
-				insn_444_read,			//8
-				modeclk_444_read,		//3
-				dummyclk_444_read,		//5
-				has_ddr_mode,			//1
 				has_3byte_addr,			//1
 				has_4byte_addr,			//1
-				capacity_mbits,			//16
+				erase_type1_insn,		//8
+				erase_type1_kbits,		//16
+				erase_type1_ms,			//16
+				erase_type2_insn,		//8
+				erase_type2_kbits,		//16
+				erase_type2_ms,			//16
+				erase_type3_insn,		//8
+				erase_type3_kbits,		//16
+				erase_type3_ms,			//16
+				erase_type4_insn,		//8
+				erase_type4_kbits,		//16
+				erase_type4_ms,			//16
+				erase_typ_to_max,		//5
+				erase_chip_ms,			//32
+				program_first_byte_us,	//7
+				program_per_byte_us,	//7
+				program_page_us,		//16
+				page_size_bytes,		//16
+				program_typ_to_max,		//5
+				busy_poll_via_flagstatreg,	//1
+				busy_poll_via_statreg,		//1
+				quad_enable_type,		//3
+				quad_qe_38,				//1
+				quad_38,				//1
+				quad_35,				//1
+				quad_6561,				//1
+				quad_6571,				//1
+				qexit_ff,				//1
+				qexit_f5,				//1
+				qexit_6571,				//1
+				qexit_reset,			//1
 
-				50'h0					//padding
+				112'h0					//padding
 			}),
 		.uart_rx(uart_rxd),
 		.uart_tx(uart_txd),
