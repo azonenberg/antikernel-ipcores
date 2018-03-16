2c85eb68228d18a61609c501958aa2bc0db4e5b2
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2018-03-15 23:30:58 -0700

    Updated 3-stage sync to use inferred FFs and be more Vivado-friendly

--- clock/crossing/ThreeStageSynchronizer.v
+++ clock/crossing/ThreeStageSynchronizer.v
@@ -3,7 +3,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2017 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2018 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -37,32 +37,26 @@ module ThreeStageSynchronizer(
 	input wire clk_in,
 	input wire din,
 	input wire clk_out,
-	output wire dout
+
+	(* ASYNC_REG = "TRUE" *)
+	output reg dout	= 0
     );
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// The flipflops
 
-	//Set false to skip the first FF stage (if input is already registered, or totally async)
-	parameter IN_REG = 1;
+	reg dout0;
+	(* ASYNC_REG = "TRUE" *) reg dout1;
 
-	wire dout0;			//The cross-clock path
-	wire dout1;
-	wire dout2;
+	//First stage: FF in the transmitting domain
+	always @(posedge clk_in) begin
+		dout0	<= din;
+	end
 
-	//First stage: either a FF in the transmitting clock domain, or nothing
-	generate
-
-		if(!IN_REG)
-			assign dout0 = din;
-		else
-			FDCE stage0 (.Q(dout0), .C(clk_in),  .CE(1'b1), .CLR(1'b0), .D(din));
-
-	endgenerate
-
-	//Three stages in the receiving clock domain
-	(* RLOC="X0Y0" *) FDCE stage1 (.Q(dout1), .C(clk_out), .CE(1'b1), .CLR(1'b0), .D(dout0));
-	(* RLOC="X0Y0" *) FDCE stage2 (.Q(dout2), .C(clk_out), .CE(1'b1), .CLR(1'b0), .D(dout1));
-	(* RLOC="X0Y0" *) FDCE stage3 (.Q(dout ), .C(clk_out), .CE(1'b1), .CLR(1'b0), .D(dout2));
+	//Two stages in the receiving clock domain
+	always @(posedge clk_out) begin
+		dout1	<= dout0;
+		dout	<= dout1;
+	end
 
 endmodule
