94534769ce3a0f561bf65eb43921dd85be5a20aa
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2019-07-18 11:45:38 -0700

    Initial implementation of X25519_Squeeze()

---
+++ crypt/x25519/X25519_Squeeze.sv
@@ -0,0 +1,67 @@
+`timescale 1ns/1ps
+`default_nettype none
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2019 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief X25519 reduction
+
+	Derived from squeeze() in NaCl crypto_scalarmult/curve25519/ref/smult.c (public domain)
+
+	Modular reduction of carry outs
+ */
+module X25519_Squeeze(
+	input wire			clk,
+	input wire			en,
+	input wire[263:0]	a,
+	output logic		out_valid	= 0,
+	output logic[263:0]	out = 0
+	);
+
+	logic			en_ff;
+	logic[254:0]	a_ff;
+	logic[31:0]		carry;
+	always_ff @(posedge clk) begin
+
+		//First pipeline stage
+		en_ff		<= en;
+		if(en) begin
+			a_ff	<= a[254:0];
+			carry	<= 19*a[263:255];
+		end
+
+		//Second pipeline stage
+		out_valid	<= en_ff;
+		if(en_ff)
+			out		<= a_ff + carry;
+	end
+
+endmodule
