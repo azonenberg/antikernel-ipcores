8f8695749154712d7a034599f3bf5b9a63e45986
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2019-08-22 00:59:29 -0700

    Corrected errors in comments

--- device_abstraction/DeviceInfo_7series.sv
+++ device_abstraction/DeviceInfo_7series.sv
@@ -3,7 +3,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2016 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2019 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -41,7 +41,7 @@ module DeviceInfo_7series(
 	//Device DNA (57 bits padded with zeros to 64)
 	output logic[63:0]	die_serial = 0,
 
-	//JTAG IDCODE (not yet implemented)
+	//JTAG IDCODE
 	output logic[31:0]	idcode = 0
 );
 
