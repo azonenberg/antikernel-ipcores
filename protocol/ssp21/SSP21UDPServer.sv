ff37b464471244b69dcacf4c509dcf7b5ba8df6c
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2019-11-28 18:52:57 -0800

    Continued implementation of SSP21 UDP server

--- protocol/ssp21/SSP21UDPServer.sv
+++ protocol/ssp21/SSP21UDPServer.sv
@@ -67,12 +67,13 @@ module SSP21UDPServer(
 	enum logic[3:0]
 	{
 		RX_STATE_IDLE			= 4'h0,
-		RX_STATE_DROP			= 4'h1,
-		RX_STATE_FIRST			= 4'h2,
-		RX_STATE_HANDSHAKE_1	= 4'h3,
-		RX_STATE_HANDSHAKE_2	= 4'h4,
-		RX_STATE_HANDSHAKE_3	= 4'h5,
-		RX_STATE_HANDSHAKE_4	= 4'h6
+		RX_STATE_FIRST			= 4'h1,
+		RX_STATE_HANDSHAKE_1	= 4'h2,
+		RX_STATE_HANDSHAKE_2	= 4'h3,
+		RX_STATE_HANDSHAKE_3	= 4'h4,
+		RX_STATE_HANDSHAKE_4	= 4'h5,
+		RX_STATE_HANDSHAKE_5	= 4'h6,
+		RX_STATE_DROP			= 4'h7
 	} rx_state = RX_STATE_IDLE;
 
 	//Type of an incoming packet
@@ -90,19 +91,48 @@ module SSP21UDPServer(
 	ssp21_handshakemode_t			rx_handshake_mode;
 	logic[255:0]					rx_handshake_nonce;
 
+	//Communications from RX to TX side
+	logic							rx_handshake_begin_en		= 0;	//handshake-begin
+	logic							rx_handshake_begin_busy		= 0;
+	logic							rx_handshake_begin_done		= 0;
+
+	logic							rx_err_en		= 0;
+	logic							rx_err_busy		= 0;
+	logic[7:0]						rx_err_id		= 0;
+	logic							rx_err_done		= 0;
+
 	logic[7:0]						rx_wcount;
 
+	localparam						OUR_PORT	= 16'd8888;
+
 	always_ff @(posedge clk) begin
+
+		rx_handshake_begin_en	<= 0;
+		rx_err_en		<= 0;
+
+		if(rx_handshake_begin_done)
+			rx_handshake_begin_busy	<= 0;
+		if(rx_err_done)
+			rx_err_busy	<= 0;
 
 		case(rx_state)
 
 			////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			// Wait for a new packet to show up
+
 			RX_STATE_IDLE: begin
 
 				//Ready for first byte of data
-				if(udp_rx_bus.start)
-					rx_state	<= RX_STATE_FIRST;
+				if(udp_rx_bus.start) begin
+
+					//Silently drop if going to the wrong port (anything but 8888 for now)
+					if(udp_rx_bus.dst_port != OUR_PORT) begin
+					end
+
+					//all good
+					else
+						rx_state	<= RX_STATE_FIRST;
+				end
 
 			end	//end RX_STATE_IDLE
 
@@ -110,11 +140,14 @@ module SSP21UDPServer(
 			// Read first word of the packet and decide how to handle it
 
 			RX_STATE_FIRST: begin
+
 				if(udp_rx_bus.data_valid) begin
 
 					//Drop the frame if truncated
-					if(udp_rx_bus.bytes_valid != 4)
+					if(udp_rx_bus.bytes_valid != 4)  begin
+						rx_err_id	<= BAD_MESSAGE_FORMAT;
 						rx_state	<= RX_STATE_DROP;
+					end
 
 					//Parse the first word
 					else begin
@@ -125,22 +158,44 @@ module SSP21UDPServer(
 
 							//Function plus start of crypto spec (version, ephemeral mode)
 							REQUEST_HANDSHAKE_BEGIN: begin
-								rx_version			<= udp_rx_bus.data[23:8];
-								rx_ephemeral_mode	<= ssp21_handshake_ephemeral_mode'(udp_rx_bus.data[7:0]);
-								rx_state			<= RX_STATE_HANDSHAKE_1;
+
+								//If it's a handshake-begin and we're still processing the previous handshake-begin,
+								//silently drop it.
+								if(rx_handshake_begin_busy)
+									rx_state			<= RX_STATE_DROP;
+								else begin
+									rx_version			<= udp_rx_bus.data[23:8];
+									rx_ephemeral_mode	<= ssp21_handshake_ephemeral_mode'(udp_rx_bus.data[7:0]);
+									rx_state			<= RX_STATE_HANDSHAKE_1;
+								end
 							end	//end REQUEST_HANDSHAKE_BEGIN
 
 						endcase
 
 					end
 				end
+
+				//Abort if the packet ends
+				if(udp_rx_bus.drop) begin
+					rx_err_id	<= BAD_MESSAGE_FORMAT;
+					rx_state	<= RX_STATE_DROP;
+				end
+
 			end	//end RX_STATE_FIRST
 
 			////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			// Drop corrupted/damaged frames
 
 			RX_STATE_DROP: begin
-				//do nothing until the frame ends
+
+				//If already sending a drop message, drop silently. Otherwise send the reply
+				if(!rx_err_busy) begin
+					rx_err_en	<= 1;
+					rx_err_busy	<= 1;
+				end
+
+				rx_state		<= RX_STATE_IDLE;
+
 			end	//end RX_STATE_DROP
 
 			////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -148,11 +203,14 @@ module SSP21UDPServer(
 
 			//Rest of crypto spec (hash, KDF, nonce mode, crypto mode)
 			RX_STATE_HANDSHAKE_1: begin
+
 				if(udp_rx_bus.data_valid) begin
 
 					//Drop the frame if truncated
-					if(udp_rx_bus.bytes_valid != 4)
-						rx_state	<= RX_STATE_DROP;
+					if(udp_rx_bus.bytes_valid != 4) begin
+						rx_err_id		<= BAD_MESSAGE_FORMAT;
+						rx_state		<= RX_STATE_DROP;
+					end
 
 					//Nope, good data
 					else begin
@@ -161,10 +219,31 @@ module SSP21UDPServer(
 						rx_nonce_mode	<= ssp21_nonce_mode'(udp_rx_bus.data[15:8]);
 						rx_crypto_mode	<= ssp21_session_crypto_mode'(udp_rx_bus.data[7:0]);
 
-						rx_state		<= RX_STATE_HANDSHAKE_2;
+						//Validate configuration
+						if(udp_rx_bus.data[31:24] != SHA256) begin
+							rx_err_id	<= UNSUPPORTED_HANDSHAKE_HASH;
+							rx_state	<= RX_STATE_DROP;
+						end
+						else if(udp_rx_bus.data[23:16] != HKDF_SHA256) begin
+							rx_err_id	<= UNSUPPORTED_HANDSHAKE_KDF;
+							rx_state	<= RX_STATE_DROP;
+						end
+						else if(udp_rx_bus.data[15:8] != INCREMENT_LAST_RX) begin
+							rx_err_id	<= UNSUPPORTED_NONCE_MODE;
+							rx_state	<= RX_STATE_DROP;
+						end
+						else
+							rx_state	<= RX_STATE_HANDSHAKE_2;
 					end
 
 				end
+
+				//Abort if the packet ends
+				if(udp_rx_bus.drop) begin
+					rx_err_id			<= BAD_MESSAGE_FORMAT;
+					rx_state			<= RX_STATE_DROP;
+				end
+
 			end	//end RX_STATE_HANDSHAKE_1
 
 			//First half of constraints (max_nonce, start of max_session_duration)
@@ -173,8 +252,10 @@ module SSP21UDPServer(
 				if(udp_rx_bus.data_valid) begin
 
 					//Drop the frame if truncated
-					if(udp_rx_bus.bytes_valid != 4)
-						rx_state	<= RX_STATE_DROP;
+					if(udp_rx_bus.bytes_valid != 4) begin
+						rx_err_id		<= BAD_MESSAGE_FORMAT;
+						rx_state		<= RX_STATE_DROP;
+					end
 
 					//Nope, good data
 					else begin
@@ -186,68 +267,204 @@ module SSP21UDPServer(
 
 				end
 
+				//Abort if the packet ends
+				if(udp_rx_bus.drop) begin
+					rx_err_id		<= BAD_MESSAGE_FORMAT;
+					rx_state		<= RX_STATE_DROP;
+				end
+
 			end	//end RX_STATE_HANDSHAKE_2
 
-			//Second half of max session duration, handshake mode, first byte of ephemeral data
+			//Second half of max session duration, handshake mode, length of ephemeral data
+			//(should always be 32 for both pubkey and nonce modes)
 			RX_STATE_HANDSHAKE_3: begin
+
 				if(udp_rx_bus.data_valid) begin
 
 					//Drop the frame if truncated
-					if(udp_rx_bus.bytes_valid != 4)
+					if(udp_rx_bus.bytes_valid != 4) begin
+						rx_err_id	<= BAD_MESSAGE_FORMAT;
 						rx_state	<= RX_STATE_DROP;
+					end
 
 					//Nope, good data
 					else begin
-						rx_max_session_duration[15:0]		<= udp_rx_bus.data[31:16];
-						rx_handshake_mode					<= ssp21_handshakemode_t'(udp_rx_bus.data[15:8]);
+						rx_max_session_duration[15:0]	<= udp_rx_bus.data[31:16];
+						rx_handshake_mode				<= ssp21_handshakemode_t'(udp_rx_bus.data[15:8]);
+
+						rx_state						<= RX_STATE_HANDSHAKE_4;
+						rx_wcount						<= 0;
+
+						//For now, we only support SHARED_SECRET
+						if(udp_rx_bus.data[15:8] != SHARED_SECRET) begin
+							rx_err_id			<= UNSUPPORTED_HANDSHAKE_MODE;
+							rx_state			<= RX_STATE_DROP;
+						end
 
 						//Read ephemeral data
 						case(rx_ephemeral_mode)
+
 							NONCE: begin
-								rx_handshake_nonce[255:248] <= udp_rx_bus.data[7:0];
+								if(udp_rx_bus.data[7:0] != 'd32) begin
+									rx_err_id	<= BAD_MESSAGE_FORMAT;
+									rx_state	<= RX_STATE_DROP;
+								end
 							end
+
+							default: begin
+								rx_err_id		<= UNSUPPORTED_HANDSHAKE_EPHEMERAL;
+								rx_state		<= RX_STATE_DROP;
+							end
+
 						endcase
 
-						rx_state							<= RX_STATE_HANDSHAKE_4;
-						rx_wcount							<= 1;
 					end
 
 				end
+
+				//Abort if the packet ends
+				if(udp_rx_bus.drop) begin
+					rx_err_id	<= BAD_MESSAGE_FORMAT;
+					rx_state	<= RX_STATE_DROP;
+				end
+
 			end	//end RX_STATE_HANDSHAKE_3
 
 			//Rest of ephemeral data
 			RX_STATE_HANDSHAKE_4: begin
-				rx_wcount	<= rx_wcount + 1;
+				if(udp_rx_bus.data_valid) begin
+
+					rx_wcount		<= rx_wcount + udp_rx_bus.bytes_valid;
+
+					if( (rx_wcount + udp_rx_bus.bytes_valid) >= 32)
+						rx_state	<= RX_STATE_HANDSHAKE_5;
+
+					case(udp_rx_bus.bytes_valid)
+						1: rx_handshake_nonce	<= { rx_handshake_nonce[247:0], udp_rx_bus.data[31:24]};
+						2: rx_handshake_nonce	<= { rx_handshake_nonce[239:0], udp_rx_bus.data[31:16]};
+						3: rx_handshake_nonce	<= { rx_handshake_nonce[231:0], udp_rx_bus.data[31:8]};
+						4: rx_handshake_nonce	<= { rx_handshake_nonce[223:0], udp_rx_bus.data[31:0]};
+					endcase
+
+				end
+
+				//Abort if the packet ends
+				if(udp_rx_bus.drop) begin
+					rx_err_id		<= BAD_MESSAGE_FORMAT;
+					rx_state		<= RX_STATE_DROP;
+				end
+
 			end	//end RX_STATE_HANDSHAKE_4
 
-		endcase
+			//Done receiving the handshake
+			RX_STATE_HANDSHAKE_5: begin
 
-		//Go back to idle after the packet is done
-		if(udp_rx_bus.commit || udp_rx_bus.drop)
-			rx_state	<= RX_STATE_IDLE;
+				//Wait for packet to complete
+				if(udp_rx_bus.commit) begin
+					//TODO
+					//rx_handshake_begin_en	<= 1;
+					//rx_handshake_begin_busy	<= 1;
+
+					//Send a bogus error to test the error reply code path
+					rx_err_id		<= BAD_MESSAGE_FORMAT;
+					rx_state		<= RX_STATE_DROP;
+				end
+
+				//Abort if the packet gets dropped
+				if(udp_rx_bus.drop) begin
+					rx_err_id		<= BAD_MESSAGE_FORMAT;
+					rx_state		<= RX_STATE_DROP;
+				end
+			end	//end RX_STATE_HANDSHAKE_5
+
+		endcase
 
 	end
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Main TX state machine
+
+	enum logic[3:0]
+	{
+		TX_STATE_IDLE			= 4'h0,
+		TX_STATE_ERROR			= 4'h1,
+		TX_STATE_ERROR_COMMIT	= 4'h2
+	} tx_state = TX_STATE_IDLE;
+
+	always_ff @(posedge clk) begin
+
+		udp_tx_bus.start		<= 0;
+		udp_tx_bus.data_valid	<= 0;
+		udp_tx_bus.bytes_valid	<= 0;
+		udp_tx_bus.commit		<= 0;
+		udp_tx_bus.drop			<= 0;
+
+		rx_handshake_begin_done	<= 0;
+		rx_err_done				<= 0;
+
+		case(tx_state)
+
+			////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// Idle, wait for something to send
+
+			TX_STATE_IDLE: begin
+
+				//Send an error message
+				if(rx_err_en) begin
+
+					udp_tx_bus.start	<= 1;
+
+					//Send to the source of the incoming packet
+					udp_tx_bus.dst_ip	<= udp_rx_bus.src_ip;
+					udp_tx_bus.dst_port	<= udp_rx_bus.src_port;
+					udp_tx_bus.src_port	<= OUR_PORT;
+
+					//Message is only 2 bytes long
+					udp_tx_bus.payload_len	<= 2;
+
+					tx_state			<= TX_STATE_ERROR;
+
+				end
+
+			end	//end TX_STATE_IDLE
+
+			////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			// Send an error message
+
+			TX_STATE_ERROR: begin
+				udp_tx_bus.data_valid	<= 1;
+				udp_tx_bus.bytes_valid	<= 2;
+				udp_tx_bus.data[31:24]	<= REPLY_HANDSHAKE_ERROR;
+				udp_tx_bus.data[23:16]	<= rx_err_id;
+				udp_tx_bus.data[15:0]	<= 0;
+				tx_state				<= TX_STATE_ERROR_COMMIT;
+			end	//end TX_STATE_ERROR
+
+			TX_STATE_ERROR_COMMIT: begin
+				udp_tx_bus.commit		<= 1;
+				rx_err_done				<= 1;
+				tx_state				<= TX_STATE_IDLE;
+			end	//end TX_STATE_ERROR_COMMIT
+
+		endcase
+	end
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Debug LA
-	/*
+
 	ila_0 ila(
 		.clk(clk),
 		.probe0(rx_state),
 		.probe1(udp_rx_bus),
-		.probe2(rx_function),
-		.probe3(rx_version),
-		.probe4(rx_ephemeral_mode),
-		.probe5(rx_hash),
-		.probe6(rx_kdf),
-		.probe7(rx_nonce_mode),
-		.probe8(rx_crypto_mode),
-		.probe9(rx_max_nonce),
-		.probe10(rx_max_session_duration),
-		.probe11(rx_handshake_mode),
-
-		.probe12(rx_wcount),
-		.probe13(rx_handshake_nonce)
-	);*/
+		.probe2(rx_err_id),
+		.probe3(rx_err_en),
+		.probe4(rx_err_busy),
+		.probe5(rx_err_done),
+		.probe6(rx_handshake_begin_en),
+		.probe7(rx_handshake_begin_busy),
+		.probe8(rx_handshake_begin_done),
+		.probe9(tx_state),
+		.probe10(udp_tx_bus)
+	);
 
 endmodule
