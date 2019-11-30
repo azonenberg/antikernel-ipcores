d789a37f025101ac47a55a749f80ab7c001bb7ea
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2019-11-30 04:41:16 -0800

    I2CMACAddressReader: added serial number support

--- interface/ethernet/I2CMACAddressReader.sv
+++ interface/ethernet/I2CMACAddressReader.sv
@@ -32,7 +32,12 @@
 `include "I2CTransceiver.svh"
 
 /**
-	@brief Logic to read an I2C EEPROM with MAC address (AT24MAC402 and compatible)
+	@brief Logic to read an I2C EEPROM with MAC address and serial number
+
+	Assumes AT24MAC402 compatible EEPROM layout:
+		Device address: 8'hb0 | ADDR_PINS
+		128-bit serial number at 0x80 - 0x8f
+		48-bit MAC address at 0x9a - 9f
  */
 module I2CMACAddressReader #(
 	parameter ADDR_PINS = 3'h0				//State of the 3 address pins on the EEPROM
@@ -48,20 +53,23 @@ module I2CMACAddressReader #(
 	output logic			ready		= 0,
 	output logic			done		= 0,
 	output logic			fail		= 0,
-	output logic[47:0]		mac			= 0
+	output logic[47:0]		mac			= 0,
+	output logic[127:0]		serial		= 0
 	);
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Register helper block
 
-	logic		open = 0;
+	logic		open 		= 0;
 	wire		reg_ready;
-	logic		select = 0;
-	logic		close = 0;
+	logic		select 		= 0;
+	logic		close 		= 0;
 	wire		rdata_valid;
 	wire[7:0]	rdata;
 	wire		burst_done;
 	wire		err;
+	logic[7:0]	eeprom_addr	= 0;
+	logic[7:0]	eeprom_burst_len	= 0;
 
 	I2CRegisterHelper #(
 		.ADDR_BYTES(1)		//2 Kbit = 256 byte eeprom
@@ -72,14 +80,14 @@ module I2CMACAddressReader #(
 		.open(open),
 		.ready(reg_ready),
 		.select(select),
-		.addr(8'h9a),		//constant address for mac addr
-		.we(1'b0),			//always reading, can't write to the mac
-		.burst_len(8'd6),	//always read entire mac addr
+		.addr(eeprom_addr),
+		.we(1'b0),						//always reading, can't write to the mac
+		.burst_len(eeprom_burst_len),
 		.close(close),
 		.rdata_valid(rdata_valid),
 		.rdata(rdata),
 		.err(err),
-		.wdata_valid(1'b0),	//can't write
+		.wdata_valid(1'b0),				//can't write
 		.wdata(8'h0),
 		.need_wdata(),
 		.burst_done(burst_done),
@@ -98,9 +106,11 @@ module I2CMACAddressReader #(
 	{
 		STATE_BOOT_WAIT		= 3'h0,
 		STATE_WAIT_ACK		= 3'h1,
-		STATE_READ			= 3'h2,
-		STATE_DONE			= 3'h3,
-		STATE_DONE_HOLD		= 3'h4
+		STATE_READ_MAC		= 3'h2,
+		STATE_MAC_DONE		= 3'h3,
+		STATE_READ_SERIAL	= 3'h4,
+		STATE_DONE			= 3'h5,
+		STATE_DONE_HOLD		= 3'h6
 	} state = STATE_BOOT_WAIT;
 
 	logic[7:0] start_count	= 1;
@@ -121,21 +131,40 @@ module I2CMACAddressReader #(
 				end
 			end	//end STATE_BOOT_WAIT
 
+			//Read the MAC address
 			STATE_WAIT_ACK: begin
 				if(reg_ready && !open) begin
-					select	<= 1;
-					state	<= STATE_READ;
+					select				<= 1;
+					eeprom_addr			<= 8'h9a;			//read mac address
+					eeprom_burst_len	<= 8'd6;
+					state				<= STATE_READ_MAC;
 				end
 			end	//end STATE_WAIT_ACK
 
-			STATE_READ: begin
+			STATE_READ_MAC: begin
 				if(rdata_valid)
 					mac		<= { mac[39:0], rdata };
 
 				if(burst_done)
+					state	<= STATE_MAC_DONE;
+
+			end	//end STATE_READ_MAC
+
+			STATE_MAC_DONE: begin
+				select				<= 1;
+				eeprom_addr			<= 8'h80;
+				eeprom_burst_len	<= 8'd16;
+				state				<= STATE_READ_SERIAL;
+			end	//end STATE_MAC_DONE
+
+			STATE_READ_SERIAL: begin
+				if(rdata_valid)
+					serial		<= { serial[119:0], rdata };
+
+				if(burst_done)
 					state	<= STATE_DONE;
 
-			end	//end STATE_READ
+			end	//end STATE_READ_MAC
 
 			STATE_DONE: begin
 				close	<= 1;
