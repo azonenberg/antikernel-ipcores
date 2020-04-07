cb1dde7cee97fc24299a95022ff0b29b179ed76e
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-04-07 00:54:07 -0700

    Clarified comment

--- protocol/udp/UDPProtocol.sv
+++ protocol/udp/UDPProtocol.sv
@@ -4,7 +4,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2018 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2020 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -48,7 +48,7 @@ module UDPProtocol(
 );
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// RX datapath
+	// RX checksum verification
 
 	wire[15:0]	rx_checksum_expected;
 
