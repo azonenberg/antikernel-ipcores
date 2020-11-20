cad8c31f55eb49643dc25ab2bc693c2509c93f74
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-11-19 23:41:19 -0800

    Continued work on IPv4 receive path. Known failure with certain packet sizes at minimum spacing (start arriving while still in RX_STATE_BODY)

--- protocol/ip-x128/v4/IPv4Protocol.sv
+++ protocol/ip-x128/v4/IPv4Protocol.sv
@@ -54,32 +54,32 @@ module IPv4Protocol(
 		RX_STATE_IDLE,
 		RX_STATE_SECOND,
 		RX_STATE_THIRD,
+		RX_STATE_BODY_FIRST,
 		RX_STATE_BODY,
 		RX_STATE_HANG
 	} rx_state = RX_STATE_IDLE;
 
-	logic[15:0]	rx_checksum_expected	= 0;
+	logic[17:0]	rx_header_checksum_stage1[2:0];
+	logic[17:0]	rx_header_checksum_stage2[1:0];
+	logic[15:0]	rx_header_checksum_stage3[1:0];
+	logic[16:0] rx_header_checksum_stage4	= 0;
+	logic[15:0] rx_header_checksum_final	= 0;
 
-	logic[17:0]	rx_header_checksum_stage1[1:0];
-	logic[16:0]	rx_header_checksum_stage2[1:0];
-
-	//Finish calculating the header checksum
-	wire[16:0] rx_header_checksum_stage3	= rx_header_checksum_stage2[0][15:0] +
-											rx_header_checksum_stage2[1][15:0] +
-											rx_header_checksum_stage2[0][16] +
-											rx_header_checksum_stage2[1][16];
-	wire[15:0] rx_header_checksum_final	= rx_header_checksum_stage3[15:0] + rx_header_checksum_stage3[16];
+	wire rx_header_checksum_ok				= (rx_header_checksum_final == 16'hffff);
 
 	EthernetRxBus	rx_bus_ff;
+	EthernetRxBus	rx_bus_ff2;
 
-	wire[15:0]	first_payload_len	= rx_bus_ff.bytes_valid - 4;
-	wire[15:0]	second_payload_len	= first_payload_len + rx_bus.bytes_valid;
+	wire[15:0]		first_payload_len	= rx_bus_ff2.bytes_valid - 4;
+	wire[15:0]		second_payload_len	= first_payload_len + rx_bus_ff.bytes_valid;
 
 	logic[15:0]		rx_bytes_left	= 0;
 
 	always_ff @(posedge rx_clk) begin
 
 		rx_bus_ff	<= rx_bus;
+		rx_bus_ff2	<= rx_bus_ff;
+
 		rx_l3_bus	<= 0;
 
 		case(rx_state)
@@ -93,7 +93,6 @@ module IPv4Protocol(
 					rx_l3_headers.payload_len	<= rx_bus.data[111:96] - 'd20;
 					rx_l3_headers.protocol		<= ipproto_t'(rx_bus.data[55:48]);
 					rx_l3_headers.src_ip		<= rx_bus.data[31:0];
-					rx_checksum_expected		<= ~rx_bus.data[47:32];
 
 					//Calculate first stage of the checksum
 					rx_header_checksum_stage1[0]	<=
@@ -104,7 +103,8 @@ module IPv4Protocol(
 						6'h05,
 						rx_bus.data[119:112]
 					} + rx_bus.data[15:0] + rx_bus.data[95:80];
-					rx_header_checksum_stage1[1]	<= rx_bus.data[111:96] + rx_bus.data[63:48] + rx_bus.data[31:16];
+					rx_header_checksum_stage1[1]	<= rx_bus.data[111:96] + rx_bus.data[63:48];
+					rx_header_checksum_stage1[2]	<= rx_bus.data[47:32] + rx_bus.data[31:16];
 
 					//Match valid IPv4 packets. Silently discard anything we don't care about.
 					if(
@@ -136,6 +136,23 @@ module IPv4Protocol(
 			//Second word of an IPv4 packet
 			RX_STATE_SECOND: begin
 
+				//Second stage of header checksum calculation
+				//Note that stage1 is 18 bits, but only the [0] can actually fill the LSB.
+				//[1] and [2] only use 16:0.
+				rx_header_checksum_stage2[0]	= rx_header_checksum_stage1[0][15:0] +
+													rx_header_checksum_stage1[1][15:0] +
+													rx_header_checksum_stage1[2][15:0];
+				rx_header_checksum_stage2[1]	= rx_bus.data[127:112] + rx_bus.data[111:96] +
+													rx_header_checksum_stage1[0][17:16] +
+													rx_header_checksum_stage1[1][16] +
+													rx_header_checksum_stage1[2][16];
+
+				//Third stage of header checksum
+				rx_header_checksum_stage3[0]	<=  rx_header_checksum_stage2[0][15:0];
+				rx_header_checksum_stage3[1]	<=  rx_header_checksum_stage2[1][15:0] +
+													rx_header_checksum_stage2[1][16] +
+													rx_header_checksum_stage2[0][16];
+
 				//As a minimum, any IPv4 packet needs to have full headers. So bail if there's not enough payload.
 				if(!rx_bus.data_valid || (rx_bus.bytes_valid < 4) ) begin
 				end
@@ -146,13 +163,6 @@ module IPv4Protocol(
 					//Extract the last header field
 					rx_l3_headers.dst_ip			<= rx_bus.data[127:96];
 
-					//Second stage of header checksum calculation
-					rx_header_checksum_stage2[0]	= rx_header_checksum_stage1[0][15:0] +
-														rx_header_checksum_stage1[1][15:0];
-					rx_header_checksum_stage2[1]	<= rx_bus.data[127:112] + rx_bus.data[111:96] +
-														rx_header_checksum_stage1[0][17:16] +
-														rx_header_checksum_stage1[1][17:16];
-
 					//Move on without actually sending any traffic to the upper layer protocol yet.
 					rx_state						<= RX_STATE_THIRD;
 
@@ -162,15 +172,26 @@ module IPv4Protocol(
 
 			RX_STATE_THIRD: begin
 
+				//Finish calculating the header checksum
+				rx_header_checksum_stage4	= rx_header_checksum_stage3[0][15:0] + rx_header_checksum_stage3[1];
+				rx_header_checksum_final	<= rx_header_checksum_stage4[15:0] + rx_header_checksum_stage4[16];
+
 				//TODO: Pseudo-header checksum
 
-				//Assume we're starting a new frame (might cancel later if header checksum is bad)
-				rx_l3_bus.start			<= 1;
+				rx_state					<= RX_STATE_BODY_FIRST;
+
+			end	//end RX_STATE_THIRD
+
+			RX_STATE_BODY_FIRST: begin
+
+				//Starting a new frame
+				rx_l3_bus.start			<= rx_header_checksum_ok;
 				rx_l3_bus.data_valid	<= 1;
+				rx_state				<= RX_STATE_BODY;
 
 				//If last cycle was the end of the packet, it was tiny.
 				//Send whatever partial data there was (after trimming off the dest IP address)
-				if(rx_bus_ff.commit) begin
+				if(rx_bus_ff2.commit) begin
 
 					//Trim off padding if we have a tiny payload
 					if(rx_l3_headers.payload_len < first_payload_len)
@@ -178,72 +199,133 @@ module IPv4Protocol(
 					else
 						rx_l3_bus.bytes_valid	<= first_payload_len;
 
-					rx_l3_bus.data			<= { rx_bus_ff.data[95:0], 32'h0 };
+					rx_l3_bus.data			<= { rx_bus_ff2.data[95:0], 32'h0 };
 
 					//Verify checksum
-					if(rx_header_checksum_final == rx_checksum_expected)
+					if(rx_header_checksum_ok)
 						rx_l3_bus.commit	<= 1;
-					else
-						rx_l3_bus.start		<= 0;
 
-					//Done
-					rx_state			<= RX_STATE_IDLE;
+					//Done this cycle
+					rx_state				<= RX_STATE_IDLE;
 
 				end
 
 				//If this cycle is the end, but we have <= 4 bytes, it's still a single cycle payload at the output,
 				//but we have to combine data from two input cycles.
-				else if(rx_bus.commit && (rx_bus.bytes_valid <= 4) ) begin
+				else if(rx_bus_ff.commit && (rx_bus_ff.bytes_valid <= 4) ) begin
 
 					//Trim off padding if we have a tiny payload
 					if(rx_l3_headers.payload_len < second_payload_len)
 						rx_l3_bus.bytes_valid	<= rx_l3_headers.payload_len;
 					else
 						rx_l3_bus.bytes_valid	<= second_payload_len;
-					rx_l3_bus.data			<= { rx_bus_ff.data[95:0], rx_bus.data[127:96] };
+					rx_l3_bus.data				<= { rx_bus_ff2.data[95:0], rx_bus_ff.data[127:96] };
 
 					//Verify checksum
-					if(rx_header_checksum_final == rx_checksum_expected)
-						rx_l3_bus.commit	<= 1;
-					else
-						rx_l3_bus.start		<= 0;
+					if(rx_header_checksum_ok)
+						rx_l3_bus.commit		<= 1;
 
-					//Done
-					rx_state			<= RX_STATE_IDLE;
+					//Done this cycle
+					rx_state					<= RX_STATE_IDLE;
 
 				end
 
-				//We have a full 16 bytes of data this cycle;
+				//We have a full 16 bytes of data this cycle.
 				else begin
 
 					//Trim off padding if we have a tiny payload
 					if(rx_l3_headers.payload_len < 16) begin
 						rx_l3_bus.bytes_valid	<= rx_l3_headers.payload_len;
-						rx_l3_bus.commit		<= 1;
 						rx_state				<= RX_STATE_IDLE;
+
+						if(rx_header_checksum_ok)
+							rx_l3_bus.commit	<= 1;
 					end
 
 					//Full length cycle
 					else begin
 						rx_l3_bus.bytes_valid	<= 16;
 						rx_bytes_left			<= rx_l3_headers.payload_len - 16;
-						rx_state				<= RX_STATE_BODY;
 					end
 
-					rx_l3_bus.data			<= { rx_bus_ff.data[95:0], rx_bus.data[127:96] };
-
-					//Verify checksum
-					if(rx_header_checksum_final != rx_checksum_expected) begin
-						rx_l3_bus.start		<= 0;
-						rx_state			<= RX_STATE_IDLE;
-					end
+					rx_l3_bus.data				<= { rx_bus_ff2.data[95:0], rx_bus_ff.data[127:96] };
 
 				end
 
-			end	//end RX_STATE_THIRD
+			end	//end RX_STATE_BODY_FIRST
 
 			RX_STATE_BODY: begin
-				//TODO
+
+				rx_l3_bus.data_valid	<= 1;
+
+				//Consume packet data
+				rx_bytes_left			<= rx_bytes_left - 16;
+
+				//If last cycle was the end of the packet, send the remaining partial data
+				if(rx_bus_ff2.commit) begin
+
+					//Trim off padding if we have a tiny payload
+					if(rx_bytes_left < first_payload_len)
+						rx_l3_bus.bytes_valid	<= rx_bytes_left;
+					else
+						rx_l3_bus.bytes_valid	<= first_payload_len;
+
+					rx_l3_bus.data			<= { rx_bus_ff2.data[95:0], 32'h0 };
+
+					//Verify checksum
+					if(rx_header_checksum_ok)
+						rx_l3_bus.commit	<= 1;
+					else
+						rx_l3_bus.drop		<= 1;
+
+					//Done this cycle
+					rx_state				<= RX_STATE_IDLE;
+
+				end
+
+				//If this cycle is the end, but we have <= 4 bytes, we have to combine data from two input cycles.
+				else if(rx_bus_ff.commit && (rx_bus_ff.bytes_valid <= 4) ) begin
+
+					//Trim off padding if we have a tiny payload
+					if(rx_bytes_left < second_payload_len)
+						rx_l3_bus.bytes_valid	<= rx_bytes_left;
+					else
+						rx_l3_bus.bytes_valid	<= second_payload_len;
+					rx_l3_bus.data				<= { rx_bus_ff2.data[95:0], rx_bus_ff.data[127:96] };
+
+					//Verify checksum
+					if(rx_header_checksum_ok)
+						rx_l3_bus.commit		<= 1;
+					else
+						rx_l3_bus.drop			<= 1;
+
+					//Done this cycle
+					rx_state					<= RX_STATE_IDLE;
+
+				end
+
+				//We have a full 16 bytes of data this cycle.
+				else begin
+
+					//Trim off padding if we have a tiny payload
+					if(rx_bytes_left < 16) begin
+						rx_l3_bus.bytes_valid	<= rx_bytes_left;
+						rx_state				<= RX_STATE_IDLE;
+
+						if(rx_header_checksum_ok)
+							rx_l3_bus.commit	<= 1;
+						else
+							rx_l3_bus.drop		<= 1;
+					end
+
+					//Full length cycle
+					else
+						rx_l3_bus.bytes_valid	<= 16;
+
+					rx_l3_bus.data				<= { rx_bus_ff2.data[95:0], rx_bus_ff.data[127:96] };
+
+				end
+
 			end	//end RX_STATE_BODY
 
 		endcase
