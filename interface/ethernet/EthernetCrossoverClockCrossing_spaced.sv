debcf06f01b17d3edb537b59f5c3cf80f4863ac4
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2023-01-22 01:32:29 -0800

    Fixed same bug in EthernetCrossoverClockCrossing_spaced

--- interface/ethernet/EthernetCrossoverClockCrossing_spaced.sv
+++ interface/ethernet/EthernetCrossoverClockCrossing_spaced.sv
@@ -3,7 +3,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2021 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2023 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -145,7 +145,7 @@ module EthernetCrossoverClockCrossing_spaced(
 	} rx_state = STATE_IDLE;
 
 	logic[3:0] rx_count			= 0;
-	logic[9:0] rx_bytes_read	= 0;
+	logic[10:0] rx_bytes_read	= 0;
 
 	always_ff @(posedge tx_clk) begin
 
