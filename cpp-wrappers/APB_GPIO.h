4504dba7d1862b0b4a99eb3fcc46e8069884a52f
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-06-27 17:21:06 -0700

    Initial version of APB_GPIO wrapper

---
+++ cpp-wrappers/APB_GPIO.h
@@ -0,0 +1,128 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL                                                                                                           *
+*                                                                                                                      *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#ifndef APB_GPIO_h
+#define APB_GPIO_h
+
+/**
+	@brief Registers for a GPIO pin
+ */
+struct __attribute__((packed)) APB_GPIO
+{
+public:
+	uint32_t		out;
+	uint32_t		in;
+	uint32_t		tris;
+};
+
+/**
+	@brief Wrapper class for APB_GPIO
+
+	Semantics largely identical to the stm32-cpp GPIOPin class
+
+	TODO: open drain support?
+ */
+class GPIOPin
+{
+public:
+
+	enum gpiomode_t
+	{
+		MODE_INPUT		= 0,
+		MODE_OUTPUT		= 1,
+		//no peripheral/analog mode supported
+	};
+
+	//no slew rate control supported
+
+	/**
+		@brief Initializes the pin
+	 */
+	GPIOPin(
+		volatile APB_GPIO* gpio,
+		uint8_t pin,
+		gpiomode_t mode)
+	: m_gpio(gpio)
+	, m_pin(pin)
+	, m_setmask(1 << pin)
+	, m_clearmask(~m_setmask)
+	{
+		//Configure the pin
+		SetMode(mode);
+	}
+
+	/**
+		@brief Set the pin to input or output mode
+	 */
+	void SetMode(gpiomode_t mode)
+	{
+		if(mode == MODE_OUTPUT)
+			m_gpio->tris &= m_clearmask;
+		else
+			m_gpio->tris |= m_setmask;
+	}
+
+	/**
+		@brief Drives a value out the pin
+	 */
+	void Set(bool b)
+	{
+		if(b)
+			m_gpio->out |= m_setmask;
+		else
+			m_gpio->out &= m_clearmask;
+	}
+
+	//Convenience helper for assigning GPIOs
+	void operator=(bool b)
+	{ Set(b); }
+
+	//Convenience helper for reading GPIOs
+	operator bool() const
+	{ return Get(); }
+
+	/**
+		@brief Reads the current value of the pin
+	 */
+	bool Get() const
+	{
+		if(m_gpio->in & m_setmask)
+			return true;
+		else
+			return false;
+	}
+
+protected:
+	volatile APB_GPIO* 	m_gpio;
+	uint8_t				m_pin;
+	uint32_t			m_setmask;
+	uint32_t			m_clearmask;
+};
+
+#endif
