129ab9f1d8a550925a899d8b0b34771b9e0b03aa
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-08-20 14:52:17 -0700

    Added APB_StatusRegister imported from trigger-crossbar

---
+++ interface/gpio/APB_StatusRegister.sv
@@ -0,0 +1,71 @@
+`timescale 1ns/1ps
+`default_nettype none
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL                                                                                                           *
+*                                                                                                                      *
+* Copyright (c) 2012-2024 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief	APB wrapper providing a 32-bit status register which can be read at any offset
+ */
+module APB_StatusRegister(
+	APB.completer 		apb,
+
+	input wire[31:0]	status
+);
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// We only support 32-bit APB, throw synthesis error for anything else
+
+	if(apb.DATA_WIDTH != 32)
+		apb_bus_width_is_invalid();
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Register logic
+
+	//Combinatorial readback and writes
+	always_comb begin
+
+		apb.pready		= apb.psel && apb.penable;
+		apb.prdata		= 0;
+		apb.pslverr		= 0;
+
+		if(apb.pready) begin
+
+			//write: alwys rejected
+			if(apb.pwrite)
+				apb.pslverr = 1;
+
+			//read: all offsets return the status value
+			else
+				apb.prdata	= status;
+
+		end
+	end
+
+endmodule
